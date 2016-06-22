983f1e4974e281e548a667f358730c451f83a300
Anna.Belousova <amb.ence@gmail.com>
2016-06-22 17:17:22 +0300

    deleted color_stretch with strange const and part of the code that was useless

--- image-processing/recognize.py
+++ image-processing/recognize.py
@@ -14,23 +14,9 @@ import sys
 
 # Angle-checking function
 def is_on_line(x1, y1, x2, y2, x3, y3):
+    # integer class variables expected
     slope = (y2 - y1) / (x2 - x1)
     return y3 - y1 == slope * (x3 - x1)
-
-# Colour values stretching function
-def color_stretch(image):
-    for i in range(image.shape[2]):
-        image = cv2.equalizeHist(image[:,:,i])
-    for index, pixel in np.ndenumerate(image):
-        if pixel < 20:
-            pixel -= 2
-        elif pixel  >230:
-            pixel +=3
-        else:
-            continue
-
-        return image
-
 
 # Color values stretching acccording to the histogram
 def histogram_stretch(image):
@@ -38,7 +24,7 @@ def histogram_stretch(image):
     for i in range(image.shape[2]):
         blue = image[:,:,i]
         blue = cv2.equalizeHist(blue)
-    image[:,:,i] = blue
+        image[:,:,i] = blue
     return image
 
 # Contours selection facilitating function
@@ -83,76 +69,8 @@ if __name__ == '__main__':
 img = cv2.imread(args['image'])  # image for analysis
 img = cv2.GaussianBlur(img,(5,5),0)  #blur is added to denoise the image
 
-# if args['panic'] == True:
-#     cv2.imshow("img", img)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
-#
-# # Convert to grayscale, shrink shapes' sizes
-# gray_thin = contours_selection_threshold(img,4)
-#
-# if args['panic'] == True:
-#     cv2.imshow("img", gray_thin)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
-#
-# ret3,th3 = cv2.threshold(gray_thin,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-# if len(np.unique(th3)) > 2:
-#     print('Something went wrong.')
-# min, max = np.unique(th3)
-#
-# if args['panic'] == True:
-#     cv2.imshow("img", th3)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
-#
-# # find the contours in the mask
-# im, cnts, hier = cv2.findContours(th3, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-#
-# #If needed: visualization of the contours found
-# if args['panic'] == True:
-#     contour_img = np.copy(img)
-#     cv2.drawContours(contour_img, cnts, -1, (0, 255, 0), thickness=3)
-#     cv2.imshow("img", img)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
-#     contour_img = np.copy(img)
-#     cv2.drawContours(contour_img, cnts, -1, (0, 255, 0), thickness=3)
-#
-# #cut the ROI
-# #finding contour areas
-# areas = []
-# for c in cnts:
-#     areas.append(cv2.contourArea(c))
-#
-# max_index = areas.index(np.max(areas))
-#
-#
-# mask = np.zeros_like(img) # Create mask where white is what we want, black otherwise
-# cv2.drawContours(mask, cnts, (max_index-1), [255,255,255], -1) # Draw filled contour in mask
-# out = np.zeros_like(img) # Extract out the object and place into output image
-# out[mask == 255] = img[mask == 255]
-# cv2.drawContours(out, cnts, -1, (0, 255, 0), thickness=3)
-#
-# if args['panic'] == True:
-#     cv2.imshow("img", out)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
-#
-# ROI = np.copy(img)
-# # # Cut the ROI
-# # x, y, w, h = cv2.boundingRect(cnts[max_index-1])
-# # ROI = img[y:(y+h),x:(x+w)]
-#
-#
-# if args['panic'] == True:
-#     cv2.imshow("img", ROI)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
-
-ROI = np.copy(img)
-# Cutting the contours in the new picture
-frame = np.copy(ROI)
+# Cutting the contours of the black frame
+frame = np.copy(img)
 ret3,th4 = cv2.threshold(frame,100,256,cv2.THRESH_BINARY)
 
 if args['panic'] == True:
@@ -160,7 +78,7 @@ if args['panic'] == True:
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-
+# Making the picture binary
 gray = cv2.cvtColor(th4, cv2.COLOR_BGR2GRAY)
 
 if args['panic'] == True:
@@ -168,9 +86,10 @@ if args['panic'] == True:
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-
+# Selecting minimum pixel value
 rango = np.unique(gray)
 
+# Creating a mask
 mask = np.zeros_like(gray) # Create mask where white is what we want, black otherwise
 mask[gray == rango[0]] = 255
 ret3,th4 = cv2.threshold(mask,100,256,cv2.THRESH_BINARY_INV)
@@ -192,7 +111,6 @@ max_index = areas.index(np.max(areas))
 
 # Selection of contours, contours mask creation
 lst_intensities = []
-
 count = 0
 for c in range(len(cnts)):
     cimg = np.zeros_like(ROI)
@@ -201,27 +119,27 @@ for c in range(len(cnts)):
     pts = np.where(cimg == [255,255,255])
     lst_intensities.append(ROI[pts[0], pts[1]])
 
+# Obtaining average intensity values
 colors_avg = []
 for i in range(len(lst_intensities)):
     colors_avg.append(np.average(lst_intensities[i], axis=0))
 
-#Checking of the colors recognized (optional)
-#for i in main_diag_coords:
+# Drawing the colours, checking the averages
+if args['panic'] == True:
+    c=0
+    check = np.empty_like(ROI)
+    while c<4:
+        for t in range(len(colors_avg)):
+            if t >= max_index-1:
+                pass
+            else:
+                color = colors_avg[t]
+                cv2.drawContours(check, cnts, t, color, thickness=-1)
+        c+=1
 
+    cv2.imshow("img", check)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    #cv2.imwrite("/Users/apple/tutorial/how_computer_sees.jpg", img)
 
-#Drawing the colours
-c=0
-check = np.empty_like(ROI)
-while c<4:
-    for t in range(len(colors_avg)):
-        if t >= max_index-1:
-            pass
-        else:
-            color = colors_avg[t]
-            cv2.drawContours(check, cnts, t, color, thickness=-1)
-    c+=1
-
-cv2.imshow("img", check)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-#cv2.imwrite("/Users/apple/tutorial/how_computer_sees.jpg", img)
+# Analyzing the white balance
