bc9281716c798d35a728d98958150621f894a93f
Anna.Belousova <amb.ence@gmail.com>
2016-07-06 17:15:21 +0300

    how to search for strings?

--- model-test/model_test.py
+++ model-test/model_test.py
@@ -42,21 +42,66 @@ def get_rs(mode, path_to_rs_list):
             return rsnp
 
 
-def boyermoorehorspool(pattern, text):
-    m = len(pattern)
-    n = len(text)
-    if m > n: return -1
-    skip = []
-    for k in range(256): skip.append(m)
-    print(skip)
-    for k in range(m - 1): skip[ord(pattern[k])] = m - k - 1
-    skip = tuple(skip)
-    k = m - 1
+# Generate the Bad Character Skip List
+def generate_bad_char_shift(term):
+    skipList = {}
+    for i in range(0, len(term) - 1):
+        skipList[term[i]] = len(term) - i - 1
+    return skipList
+
+
+# Generate the Good Suffix Skip List
+def findSuffixPosition(badchar, suffix, full_term):
+    for offset in range(1, len(full_term) + 1)[::-1]:
+        flag = True
+        for suffix_index in range(0, len(suffix)):
+            term_index = offset - len(suffix) - 1 + suffix_index
+            if term_index < 0 or suffix[suffix_index] == full_term[term_index]:
+                pass
+            else:
+                flag = False
+        term_index = offset - len(suffix) - 1
+        if flag and (term_index <= 0 or full_term[term_index - 1] != badchar):
+            return len(full_term) - offset + 1
+
+
+def generateSuffixShift(key):
+    skipList = {}
+    buffer = ""
+    for i in range(0, len(key)):
+        skipList[len(buffer)] = findSuffixPosition(key[len(key) - 1 - i], buffer, key)
+        buffer = key[len(key) - 1 - i] + buffer
+    return skipList
+
+
+# Actual Search Algorithm
+def BMSearch(haystack, needle):
+    goodSuffix = generateSuffixShift(needle)
+    badChar = generate_bad_char_shift(needle)
+    i = 0
+    while i < len(haystack) - len(needle) + 1:
+        j = len(needle)
+        while j > 0 and needle[j - 1] == haystack[i + j - 1]:
+            j -= 1
+        if j > 0:
+            badCharShift = badChar.get(haystack[i + j - 1], len(needle))
+            goodSuffixShift = goodSuffix[len(needle) - j]
+            if badCharShift > goodSuffixShift:
+                i += badCharShift
+            else:
+                i += goodSuffixShift
+        else:
+            return i
+    return -1
+
     while k < n:
-        j = m - 1; i = k
+        j = m - 1
+        i = k
         while j >= 0 and text[i] == pattern[j]:
-            j -= 1; i -= 1
-        if j == -1: return i + 1
+            j -= 1
+            i -= 1
+        if j == -1:
+            return i + 1
         k += skip[ord(text[k])]
     return -1
 
@@ -79,35 +124,15 @@ if __name__ == '__main__':
     for file in os.listdir(os.path.abspath(v)):
         path = os.getcwd()
         os.chdir(v)
-        with open(file, 'r', encoding='cp1252') as vicief:
-            for q, line in enumerate(vicief):
-                for i in snp:
-                    text = line
-                    pattern = i
-                    s = boyermoorehorspool(pattern, text)
-                    print('Text:', text)
-                    print('Pattern:', pattern)
-                    if s > -1:
-                        print('Pattern' + pattern + 'found at position', s )
-
-                        # bs["{0}".format(i)] = re.match(i, line)
+        vicief = open(file, 'r', encoding='cp1252')
+        for q, line in enumerate(vicief):
+            for i in snp:
+                print(line)
+                print(i)
+                bs["{0}".format(i)] = BMSearch(line, i)
         os.chdir(path)
         c += 1
-            # for line in vicief:
-            #     for i in snp:
-            #        if 'i' in line:
-            #            print(i, line)
-        # with open(file, 'rb', 0) as tex, \
-        #         mmap.mmap(tex.fileno(), 0, access=mmap.ACCESS_READ) as s:
-        #     for i in snp:
-        #        if s.find(b'i') != -1:
-        #            print(s)
 
-        # f = open(file, mode='rt', encoding='cp1252')
-        # for q, line in enumerate(f):
-        #    for i in snp:
-        #         bs["{0}".format(i)] = re.match(i, line)
-        # print(answer)
     print(bs)
     print(c)
     print(snp)
