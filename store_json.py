bd4e78f884db9c0aa335f85791c3d2fcda7b34e4
Anna.Belousova <amb.ence@gmail.com>
2016-08-30 12:27:38 +0300

    deleted unnecessary

--- store_json.py
+++
@@ -1,7 +0,0 @@
-from collections import defaultdict
-import json
-
-accuracy = {'blue': 0.94, 'intermed': 0.74, 'brown': 0.95}
-
-with open('/Users/apple/digital-phenotyping/self-report/eye_accuracy.json', 'w') as fp:
-    json.dump(accuracy, fp, sort_keys=True, indent=4)
